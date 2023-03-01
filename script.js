fec48a91738abb6333a1a542e52da256c623a6c1
Kim hyung wook <104712714+skanerd@users.noreply.github.com>
2023-03-01 15:26:23 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// window.sr = ScrollReveal({ reset: true });
+
+// sr.reveal("#animation-left", {
+//   origin: "left",
+//   duration: 1000,
+//   distance: "10rem",
+//   delay: 200,
+// });
+
+// sr.reveal("#animation-right", {
+//   origin: "right",
+//   duration: 1000,
+//   distance: "10rem",
+//   delay: 200,
+// });
+
+
+
+// ↑↑↑ ライブラリと一緒に使用。 ↑↑↑
+// スクロールが動かなくなる。
